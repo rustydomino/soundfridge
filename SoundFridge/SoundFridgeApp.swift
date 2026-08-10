7eddfe1664ff5b6c58cc1b1f2f3e1661bdd3302a
Mike Chao <aeries.worm.0i@icloud.com>
2026-08-10 00:29:48 -0700

    Add SoundFridge Xcode app project

---
+++ SoundFridge/SoundFridgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoundFridgeApp.swift
+//  SoundFridge
+//
+//  Created by Mike Chao on 8/10/26.
+//
+
+import SwiftUI
+
+@main
+struct SoundFridgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
